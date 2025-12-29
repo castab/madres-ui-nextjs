a704ea6195a38bdf254e81e9fd850354bcf51d63
Brayan Castaneda <32408642+castab@users.noreply.github.com>
2025-12-28 19:58:17 -0800

    Use new base rate config route

--- lib/options.ts
+++ lib/options.ts
@@ -21,11 +21,11 @@ export async function fetchOptions(type: string): Promise<Option[]> {
 }
 
 export async function fetchBaseRate(): Promise<number> {
-  const json = await fetch(`${process.env.BACKEND_URL}/options/base`, {
+  const json = await fetch(`${process.env.BACKEND_URL}/config/base_rate`, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: process.env.BACKEND_TOKEN as string,
     },
   }).then((response) => response.json())
-  return json as number
+  return json.base_rate as number
 }
